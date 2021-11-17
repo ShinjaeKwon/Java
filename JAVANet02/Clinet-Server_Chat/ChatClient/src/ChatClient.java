ca84d66f2ddd738ee5c92c2a22e828e52431bf90
JaeKwonShin <sjk6437@gmail.com>
2021-11-17 11:39:16 +0900

    TCP기반 채팅 (클라이언트)

--- JAVANet02/Clinet-Server_Chat/ChatClient/src/ChatClient.java
+++ JAVANet02/Clinet-Server_Chat/ChatClient/src/ChatClient.java
@@ -23,11 +23,50 @@ public class ChatClient {
         System.out.print("Name : ");
         String name = scn.nextLine();
         dos.writeUTF(name); //클라이언트 이름을 서버에 전송
-        while (true){
-            char q= scn.next().charAt(0);
-            if(q == 'q'){
-                break;
+        //전송쓰레드 생성
+        Thread sending = new Thread(new Runnable() {
+            @Override
+            public void run() { //익명 클래스 기법
+                while (true){
+                    System.out.println("Msg(who#message) : ");
+                    String msg = scn.nextLine();
+                    try {
+                        dos.writeUTF(msg); //전송
+                        if(msg.equals("Bye")) break; //종료
+                    } catch (IOException e) {
+                      //  e.printStackTrace();
+                    }
+
+                }
+                try {
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
+        });
+        //수신쓰레드 생성
+        Thread receiving = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        String msg = dis.readUTF();
+                        if (msg == null) break; //상대방이 접속 종료
+                        System.out.println(msg);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        //쓰레드 동작
+        sending.start();
+        receiving.start();
     }
 }
